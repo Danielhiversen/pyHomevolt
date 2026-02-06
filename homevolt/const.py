97fae459b00f6fcad9e612e97b43e9cab18b56c1
Daniel Hjelseth Høyer <github@dahoiv.net>
2026-02-06 21:51:48 +0100

    Shcedule type (#5)
    * schedule type
    
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>
    
    * schedule type
    
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>
    
    ---------
    
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>

--- homevolt/const.py
+++ homevolt/const.py
@@ -6,17 +6,18 @@ ENDPOINT_SCHEDULE = "/schedule.json"
 ENDPOINT_CONSOLE = "/console.json"
 ENDPOINT_PARAMS = "/params.json"
 
+# Map integer schedule codes to snake_case identifiers
 SCHEDULE_TYPE = {
-    0: "Idle",
-    1: "Inverter Charge",
-    2: "Inverter Discharge",
-    3: "Grid Charge",
-    4: "Grid Discharge",
-    5: "Grid Charge/Discharge",
-    6: "Frequency Reserve",
-    7: "Solar Charge",
-    8: "Solar Charge/Discharge",
-    9: "Full Solar Export",
+    0: "idle",
+    1: "inverter_charge",
+    2: "inverter_discharge",
+    3: "grid_charge",
+    4: "grid_discharge",
+    5: "grid_charge_discharge",
+    6: "frequency_reserve",
+    7: "solar_charge",
+    8: "solar_charge_discharge",
+    9: "full_solar_export",
 }
 
 # Device type mappings for sensors
